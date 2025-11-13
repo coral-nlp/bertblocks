Fix missing layernorm scaling after the final layer

bertblocks/modeling/model.py
@@ -162,7 +162,7 @@ class BertBlocksModel(BertBlocksPreTrainedModel):
         super().__init__(config)
         self.embd = TokenEmbedding(config)
         self.encd = Encoder(config)
-        self.norm = _get_norm_module(config, config.num_blocks) if config.include_final_norm else nn.Identity()
+        self.norm = _get_norm_module(config, "both", config.num_blocks) if config.include_final_norm else nn.Identity()
         self.pool = Pooler(config) if add_pooling_layer else None
         self.pad_token_id = config.pad_token_id or 0
         self.post_init()

bertblocks/modeling/norms.py
@@ -161,7 +161,7 @@ def get_norm(config: "BertBlocksConfig") -> "nn.Module":
 
 
 def _get_norm_module(config: "BertBlocksConfig",
-                     norm_kind: Literal["pre", "post"],
+                     norm_kind: Literal["pre", "post", "both"],
                      layer_id: int) -> nn.Module:
     """
     Get the appropriate normalization module for pre or post normalization based on the given config.