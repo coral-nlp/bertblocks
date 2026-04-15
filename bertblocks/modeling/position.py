ab8688a07f21d7ce8f82ce6497ddfa090a16486a
Ferdinand Schlatt <fschlatt@gmail.com>
2026-04-15 10:42:56 +0200

    remove unused variables

--- bertblocks/modeling/position.py
+++ bertblocks/modeling/position.py
@@ -48,7 +48,7 @@ if is_flash_attn_2_available():
         return dx, None, None, None, None, None, None
 
     def apply_rotary_setup_context(ctx, inputs, output):  # type: ignore
-        x, cos, sin, interleaved, cu_seqlens, max_seqlen, attention_mask = inputs
+        _, cos, sin, interleaved, cu_seqlens, max_seqlen, _ = inputs
         ctx.save_for_backward(cos, sin, cu_seqlens)
         ctx.interleaved = interleaved
         ctx.max_seqlen = max_seqlen
